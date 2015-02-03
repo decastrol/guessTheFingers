2f2cb803736d76b37114204c0a1685638833d064
Luke <ldecastro2601@gmail.com>
2015-02-02 21:58:03 -0500

    score tracking added/ now saves after app closes

--- Guess the Fingers/ViewController.swift
+++ Guess the Fingers/ViewController.swift
@@ -7,22 +7,32 @@
 //
 
 import UIKit
+
 var randomNumber = Int(arc4random() % 100)
 var guessesNumber = 0
 class ViewController: UIViewController {
     
 
+    @IBOutlet var guessButtonObject: UIButton!
+    @IBOutlet var compScore: UILabel!
+    @IBOutlet var youScore: UILabel!
     @IBOutlet var numberOfGuessesLabel: UILabel!
     @IBOutlet var guessesLabel: UILabel!
     @IBOutlet var userGuess: UITextField!
     @IBOutlet var outputLabel: UILabel!
+    @IBOutlet var switchToComputerButton: UIButton!
+    @IBAction func switchToComputer(sender: AnyObject) {
+    }
     
+    var computerPoints = 0
+    var humanPoints = 0
     
     @IBAction func guessButton(sender: AnyObject)
     {
         println("\(randomNumber)")
         guessesLabel.hidden = false
         var guess = userGuess.text.toInt()
+        if (guess < 101  && guess > -1){
         if (guess > randomNumber){
             outputLabel.text = "Too High"
             userGuess.text = nil
@@ -36,33 +46,63 @@ class ViewController: UIViewController {
             numberOfGuessesLabel.text = "\(guessesNumber)"
         }
         else if (guess == randomNumber) {
+            ++guessesNumber
             outputLabel.text = "You guessed the correct number"
+            numberOfGuessesLabel.text = String(guessesNumber)
+            
             var resetAlert = UIAlertView()
             resetAlert.title = "You Guessed It"
-            resetAlert.message = "You may now reset the game\nNumber of Guesses: \(guessesNumber + 1)"
-            resetAlert.addButtonWithTitle("Reset Game")
+            resetAlert.message = "You may now switch places\nNumber of Guesses: \(guessesNumber)"
+            resetAlert.addButtonWithTitle("Got it")
             resetAlert.show()
             userGuess.text = nil
-            randomNumber = Int(arc4random() % 100)
+            
             outputLabel.text = "Now switch places"
-            guessesNumber = 0
-            numberOfGuessesLabel.text = "0"
             userGuess.resignFirstResponder()
+            self.switchToComputerButton.hidden = false
+            guessButtonObject.hidden = true
+        }
+    }
+        else {
+            userGuess.resignFirstResponder()
+            outputLabel.text = "Not A Valid Guess"
+            userGuess.text = nil
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        guessesNumber = 0
+        
+        self.compScore.text = String(computerPoints)
+        self.youScore.text = String(humanPoints)
+        NSUserDefaults.standardUserDefaults().setObject(computerPoints, forKey: "computer points")
+        NSUserDefaults.standardUserDefaults().setObject(humanPoints, forKey: "user points")
+        
+        computerPoints = NSUserDefaults.standardUserDefaults().objectForKey("computer points") as Int
+        humanPoints = NSUserDefaults.standardUserDefaults().objectForKey("user points") as Int
+        
         var startAlert = UIAlertView()
         startAlert.title = "Guess the Fingers!"
-        startAlert.message = "You have to guess how many fingers I have from 1-100. (I have a lot of fingers)\nSee how many guesses you can get!"
+        startAlert.message = "You have to guess how many fingers I have from 0-100. (I have a lot of fingers)\nSee how many guesses you can get!"
         startAlert.addButtonWithTitle("Ok, got it")
         startAlert.show()
         guessesLabel.hidden = true
+        self.switchToComputerButton.hidden = true
+        
+        randomNumber = Int(arc4random() % 100)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "showComputer") {
+            var compVc = segue.destinationViewController as computerViewController
+            compVc.userGuesses = guessesNumber
+            compVc.mePoints = humanPoints
+            compVc.compPoints = computerPoints
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Guess the Fingers/computerViewController.swift
+++ Guess the Fingers/computerViewController.swift
@@ -10,7 +10,6 @@ import Foundation
 import UIKit
 
 class computerViewController: UIViewController {
-
     
     @IBOutlet var computerScore: UILabel!
     @IBOutlet var userScore: UILabel!
@@ -19,41 +18,67 @@ class computerViewController: UIViewController {
     @IBOutlet var computerOutput: UILabel!
     @IBOutlet var numberOfComputerGuesses: UILabel!
     @IBOutlet var numberPutter: UIButton!
+    @IBOutlet var switchToUserButton: UIButton!
+    @IBAction func switchToUser(sender: AnyObject) {
+    }
+    
+    var userGuesses: Int = 0
+    var compPoints:Int = 0
+    var mePoints: Int = 0
     
     @IBAction func putInAnswer(sender: AnyObject) {
-        self.resignFirstResponder()
+        userGivesAnswer.resignFirstResponder()
         self.numberPutter.hidden = true
         var answer = self.userGivesAnswer.text.toInt()
         var computerNumberGuess = 50
         var guessIncrement = 50
         var guessCounter = 0
-        
+        if (answer > 0 && answer < 101){
         while computerNumberGuess != answer{
-            
             if (computerNumberGuess > answer) {
                 self.computerGuess.text = String(computerNumberGuess)
                 self.computerOutput.text = "Huh, too high!"
                 ++guessCounter
                 self.numberOfComputerGuesses.text = String(guessCounter)
-                guessIncrement = guessIncrement/2
+                
+                var floatGuessIncrement:Double = Double(guessIncrement)
+                floatGuessIncrement = ceil(floatGuessIncrement/2)
+                guessIncrement = Int(floatGuessIncrement)
+//                println("\(guessIncrement)")
                 computerNumberGuess = computerNumberGuess - guessIncrement
+                println("\(computerNumberGuess)")
             }
             else if (computerNumberGuess < answer) {
                 self.computerOutput.text = "Oh, too low"
                 self.computerGuess.text = String(computerNumberGuess)
                 ++guessCounter;
                 self.numberOfComputerGuesses.text = String(guessCounter)
-                guessIncrement = guessIncrement/2
+                var floatGuessIncrement:Double = Double(guessIncrement)
+                floatGuessIncrement = ceil(floatGuessIncrement/2)
+                guessIncrement = Int(floatGuessIncrement)
+                println("\(computerNumberGuess)")
                 computerNumberGuess = computerNumberGuess + guessIncrement
             }
-            //var delayTimer: NSTimer =
         }
+        
         
         if (computerNumberGuess == answer) {
             self.computerOutput.text = "Got It!"
+            userGivesAnswer.resignFirstResponder()
             self.computerGuess.text = String(computerNumberGuess)
             ++guessCounter
             self.numberOfComputerGuesses.text = String(guessCounter)
+            
+            if (self.userGuesses > guessCounter){
+                ++compPoints
+                self.computerScore.text = String(compPoints)
+                NSUserDefaults.standardUserDefaults().setObject(compPoints, forKey: "computer points")
+            }
+            else if(self.userGuesses < guessCounter){
+                ++mePoints
+                NSUserDefaults.standardUserDefaults().setObject(mePoints, forKey: "user points")
+                self.userScore.text = String(mePoints)
+            }
             
             var switchAlert = UIAlertView()
             switchAlert.title = "The Phone Guessed It!"
@@ -61,15 +86,44 @@ class computerViewController: UIViewController {
             switchAlert.addButtonWithTitle("Okay")
             switchAlert.show()
             
+            userGivesAnswer.resignFirstResponder()
+            switchToUserButton.hidden = false
             guessCounter = 0
             guessIncrement = 50
         }
+        }
+        else {
+            userGivesAnswer.resignFirstResponder()
+            computerOutput.text = "Not Valid Number"
+            numberPutter.hidden = false
+            userGivesAnswer.text = nil
+        }
+        
     }
     
+        
     override func viewDidLoad() {
+        
+        var startComputerAlert = UIAlertView()
+        startComputerAlert.title = "Computer Turn"
+        startComputerAlert.message = "Put a number in for your phone to guess!"
+        startComputerAlert.addButtonWithTitle("Okay")
+        startComputerAlert.show()
+        
+        self.userScore.text = String(mePoints)
+        self.computerScore.text = String(compPoints)
         
         self.computerGuess.text = ""
         self.numberPutter.hidden = false
+        switchToUserButton.hidden = true
         
     }
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "showUser") {
+            var normalVc = segue.destinationViewController as ViewController
+            normalVc.humanPoints = mePoints
+            normalVc.computerPoints = compPoints
+        }
+    }
+    
 }
